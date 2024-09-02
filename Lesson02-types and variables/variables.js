af700d93a0655ca79645aa248fe9fa07d76d9ffd
Omotola Jinadu <91120863+tolagenius1123@users.noreply.github.com>
2024-09-02 19:11:46 +0100

    completed tutorial on variables

---
+++ Lesson02-types and variables/variables.js
@@ -0,0 +1,31 @@
+// Variable Declaration
+// let firstname;
+// let lastname;
+
+// Variable Assignment
+// firstname = "Tola";
+// lastname = "Jina";
+
+//Variable Declaration and Assignment
+// let firstname = "Cynthia";
+// const lastname = "James";
+
+// Reassigned a value to firstname
+// firstname = 10;
+// console.log(firstname);
+
+// Strings Data Type
+let fullname = "Omotola Jinadu";
+let houseAddress = "16 Isaac John street, Lagos";
+let occupation = "Software Engineer";
+
+// Number Data Type
+let age = 30;
+
+// String Concatenation
+// let result = "My name is " + fullname + " and I am a " + occupation;
+// console.log(result);
+
+// Template Literals (``) => refered to as backticks
+let result = `My name is ${fullname} and I am a ${occupation}`;
+console.log(result);
