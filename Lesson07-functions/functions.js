a06f61dc29bf2f074ed6272863743636689f2f72
Omotola Jinadu <91120863+tolagenius1123@users.noreply.github.com>
2024-09-23 17:48:30 +0100

    taught students about functions in js

---
+++ Lesson07-functions/functions.js
@@ -0,0 +1,31 @@
+// Function is a block of code designed to perform a specific task
+
+// Functions consist of
+// 1. Function keyword
+// 2. Function name
+// 3. Function body
+
+// Regular function
+// function displayName(name) {
+// 	console.log(name);
+// }
+
+// Arrow Function
+// const displayName = (name) => {
+// 	console.log(name);
+// };
+
+// displayName("Eva");
+// displayName("Nancy");
+// displayName("Josh");
+
+// function addTwoNumbers(num1, num2) {
+// 	console.log(num1 + num2);
+// }
+
+const addTwoNumbers = (num1, num2, num3) => {
+	console.log(num1 + num2 + num3);
+};
+
+addTwoNumbers(5, 10, 25);
+addTwoNumbers(25, 25, 10);
